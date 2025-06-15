dbd0b9f4253bd4bafbfe12597293b1afe5afc3da
laingyy <laing1324@gmail.com>
2025-06-15 18:40:41 +0100

    Add localization support: configure resource files, update persistence project, and integrate localization services.

--- Persistance/Services/Extensions/PersistanceServiceCollectionExtension.cs
+++ Persistance/Services/Extensions/PersistanceServiceCollectionExtension.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Persistance.Database;
 
@@ -14,12 +16,25 @@ public static class PersistanceServiceCollectionExtension
     public static IServiceCollection AddPersistance(this IServiceCollection services)
     {
         // Add persistence services here
+        // adding database services
         services.AddDbContextPool<LeagueDbContext>(opt =>
         {
             opt.UseNpgsql(
                 Environment.GetEnvironmentVariable("CONNECTION_STRING"),
                 b => b
                     .SetPostgresVersion(13, 0));
+        });
+
+        // adding localization services
+        services.AddLocalization(opt =>
+            opt.ResourcesPath = @"Language\Resources\");
+
+        services.Configure<RequestLocalizationOptions>(opt =>
+        {
+            var supported = new[] { new CultureInfo("en-US"), new CultureInfo("fr-FR") };
+            opt.SetDefaultCulture("en");
+            opt.SupportedCultures = supported;
+            opt.SupportedUICultures = supported;
         });
         
         return services;
