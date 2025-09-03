4fc8b22a8f0bd6e697247102de6c6fbe6b58324c
laingyy <laing1324@gmail.com>
2025-09-03 17:00:46 +0100

    Add `DiscordContext` and database service configuration
    - Create `DiscordContext` class for EF Core integration.
    - Add EF Core and Npgsql package references to `Persistance` project.
    - Introduce `DbServices` with `ConfigureDbServices` method for database dependency setup.

---
+++ Persistance/Database/DiscordContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistance.Database;
+
+public class DiscordContext : DbContext
+{
+    public DiscordContext(DbContextOptions<DiscordContext> options) 
+        : base(options)
+    {
+        
+    }
+}
---
+++ Persistance/Database/Services/DbServices.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistance.Database.Services;
+
+public class DbServices
+{
+    public static void ConfigureDbServices(IServiceCollection service)
+    {
+        service.AddNpgsql<DiscordContext>()
+    }
+}
