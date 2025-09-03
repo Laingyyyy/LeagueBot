4882fc33d0c30fdf47367217e6db4b5f4b2845fd
laingyy <laing1324@gmail.com>
2025-09-03 16:06:32 +0100

    Add core bot functionality and service configurations
    - Implement `Bot` class with Discord client setup, service configuration, and command/event handling.
    - Add `DiscordServices`, `PersistanceServices`, and `SharedServices` with basic `ConfigureServices` methods.
    - Introduce `TokenNotFound` exception handling.
    - Update `Application` project to integrate and run the `Bot`.
    - Add necessary project references and package dependencies for all components.

---
+++ Bot/Exceptions/TokenNotFound.cs
@@ -0,0 +1,23 @@
+﻿namespace LeagueBot.Exceptions;
+
+public class TokenNotFound : Exception
+{
+    public string key { get; private set; }
+    
+    public TokenNotFound(string key)
+    {
+        this.key = key;
+    }
+
+    public TokenNotFound(string key, string message)
+        : base(message)
+    {
+        this.key = key;
+    }
+    
+    public TokenNotFound(string key, string message, Exception inner)
+        : base(message, inner)
+    {
+        this.key = key;
+    }
+}
---
+++ Shared/Services/SharedServices.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Services;
+
+public class SharedServices
+{
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        
+    }
+}
